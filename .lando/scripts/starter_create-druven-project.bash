5e9d634f1cb01bd17d96dbb15797e663c507dbb6
Ronan Ribeiro <ronan@7links.com.br>
2024-01-24 13:01:27 -0300

    main: question if you want start lando

--- .lando/scripts/starter_create-druven-project.bash
+++ .lando/scripts/starter_create-druven-project.bash
@@ -11,10 +11,31 @@ cp ../resources/.lando.base.yml ../../.lando.base.yml || exit;
 read -p "Enter the new project name (ex: druven_lando-starter10): $BR" project
 sed -i "s/{DRUVEN-LANDO-STARTER}/$project/g" ../../.lando.base.yml || exit;
 
+# Question
+while true; do
+  read -p "Would you like to run the 'lando start' command: \"$(basename "$0")\"? [${YESWORD} / ${NOWORD}]? " yn
+
+  # Check if the variable is empty or undefined
+  if [ -z "$yn" ]; then
+      yn="Y"
+  fi
+
+  if [[ "$yn" =~ $YESEXPR ]]; then
+    echo_msg "Starting 'lando stat' command";
+    lando start;
+  fi
+  # Do NOT
+  if [[ "$yn" =~ $NOEXPR ]]; then
+    echo "You can run the 'lando start' command whenever you want.";
+  fi
+  exit;
+done
+
 echo_msg "Removing create-druven-project.bash launch script";
 rm -fr ../../create-druven-project.bash || exit;
 
 echo_msg "Done!";
+
 
 # @improve Add php version selection
 # @improve Add mariadb version selection
