3c1617e9a4a0575cf4341810a422fbd0fe927d8c
Ronan Ribeiro <ronan@7links.com.br>
2024-01-24 08:11:57 -0300

    main: uses a different approach for the .lando file

--- .lando/scripts/starter_create-project.bash
+++ .lando/scripts/starter_create-project.bash
@@ -4,12 +4,12 @@ source "$(dirname "$0")"/helper_starter.bash;
 
 echo_step "Starting the creation of a new druven lando project.";
 
-# Create a new ../.lando.yml file base on ../resources/.lando.base.yml
-cp ../resources/.lando.base.yml ../.lando.yml;
+# Create a new ../.lando.base.yml file base on ../resources/.lando.base.yml
+cp ../resources/.lando.base.yml ../.lando.base.yml
 
 # @todo: Validate project name input
 read -p "Enter the new project name (ex: druven_lando-starter10): $BR" project
-sed -i "s/{DRUVEN-LANDO-STARTER}/$project/g" ../.lando.yml;
+sed -i "s/{DRUVEN-LANDO-STARTER}/$project/g" ../.lando.base.yml;
 
 # @improve Add php version selection
 # @improve Add mariadb version selection
