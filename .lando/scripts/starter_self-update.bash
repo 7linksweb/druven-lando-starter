d759b7fb3c840e807ac4473fb3a1f675528813b2
Ronan Ribeiro <ronan@7links.com.br>
2024-01-24 08:05:35 -0300

    main: init starter self-update

---
+++ .lando/scripts/starter_self-update.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source "$(dirname "$0")"/helper_starter.bash;
+
+#echo_step "Starting druven lando starter self-update.";
+# @todo Implements
+echo_step "It needs to be implemented. No action was taken.";
+
+
