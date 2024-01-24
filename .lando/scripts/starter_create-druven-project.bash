74fe2eb056aeb27c01dede0b959f0c01464171d5
Ronan Ribeiro <ronan@7links.com.br>
2024-01-24 12:44:20 -0300

    main: Fix paths issues

--- .lando/scripts/starter_create-druven-project.bash
+++ .lando/scripts/starter_create-druven-project.bash
@@ -4,17 +4,17 @@ source "$(dirname "$0")"/helper_starter.bash || exit;
 
 echo_step "Starting the creation of a new druven lando project.";
 
-echo_msg "Create a new ../.lando.base.yml file base on ../resources/.lando.base.yml";
-cp ../resources/.lando.base.yml ../.lando.base.yml || exit;
+echo_msg "Create a new ./.lando.base.yml file base on ../resources/.lando.base.yml";
+cp ../resources/.lando.base.yml ../../.lando.base.yml || exit;
 
 # @todo: Validate project name input
 read -p "Enter the new project name (ex: druven_lando-starter10): $BR" project
-sed -i "s/{DRUVEN-LANDO-STARTER}/$project/g" ../.lando.base.yml;
+sed -i "s/{DRUVEN-LANDO-STARTER}/$project/g" ../../.lando.base.yml || exit;
 
+echo_msg "Removing create-druven-project.bash launch script";
+rm -fr ../../create-druven-project.bash || exit;
 
-echo_msg "Create druven project launch script";
-rm -fr ../create-druven-project.bash
-
+echo_msg "Done!";
 
 # @improve Add php version selection
 # @improve Add mariadb version selection
